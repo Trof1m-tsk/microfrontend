3321365a08998e7266a20278814994e74ffc2d4b
Mikhail Trofimov <m.trofimov1987@gmail.com>
2023-08-14 22:15:47 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  console.log('>>> MarketingApp', MarketingApp);
+  return (
+    <div>
+      <h1>Micro-frontend Application!</h1>
+      <hr/>
+      <MarketingApp />
+    </div>
+  )
+}
---
+++ packages/container/src/bootstrap.js
@@ -0,0 +1,8 @@
+import App from './App';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
