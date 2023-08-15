351d0571e65d434aba812be32624fe9947493d8d
Mikhail Trofimov <m.trofimov1987@gmail.com>
2023-08-15 19:43:43 +0200

    Small changes to trigger deployment process final

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+  console.log('>>> MarketingApp', MarketingApp);
   return (
     <div>
       <h1>Micro-frontend Application!!!</h1>
