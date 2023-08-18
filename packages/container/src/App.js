722f52191643e3d72f049b682be5eec9f4b5d311
Mikhail Trofimov <m.trofimov1987@gmail.com>
2023-08-18 16:58:20 +0200

    Commit 016

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Micro frontend Application!!!</h1>
+      <h1>Micro-frontend Application!!!</h1>
       <hr/>
       <MarketingApp />
     </div>
