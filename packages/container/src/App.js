571aad4f21e59afa04048607f206598ed0a2a9db
Mikhail Trofimov <m.trofimov1987@gmail.com>
2023-08-22 10:51:24 +0200

    Header added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
   return (
-    <div>
-      <h1>Micro-frontend Application!!!</h1>
-      <hr/>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <h1>Micro-frontend Application!!!</h1>
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
